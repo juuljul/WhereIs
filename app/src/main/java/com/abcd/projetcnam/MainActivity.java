0dd1bef3a24b00192e256acd2e17ffaaa566cf78
julienb <julien.bremeersch@gmail.com>
2016-02-21 14:45:05 +0100

    Added material design to main

--- app/src/main/java/com/abcd/projetcnam/MainActivity.java
+++ app/src/main/java/com/abcd/projetcnam/MainActivity.java
@@ -2,20 +2,58 @@ package com.abcd.projetcnam;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        navigationView = (NavigationView) findViewById(R.id.navigationView);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.trajetSearch:
+                        Intent intent = new Intent(getApplicationContext(),LocationActivity.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.scheduleSearch:
+                        Intent intent2 = new Intent(getApplicationContext(),ScheduleActivity.class);
+                        startActivity(intent2);
+                        break;
+                }
+                return true;
+            }
+        });
+
+        drawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.setDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
     }
 
 
